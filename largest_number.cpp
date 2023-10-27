ccb90daae18989b3f11221c155067bb7384ed167
vibhorkudeshiya <142561081+vibhorkudeshiya@users.noreply.github.com>
2023-10-27 19:12:41 +0530

    Create largest_number.cpp

---
+++ largest_number.cpp
@@ -0,0 +1,43 @@
+/*
+PRN - 22070123132
+
+C++ Program to Find Largest Among
+Three Numbers Using if-else
+Statement
+
+*/
+
+
+#include <iostream>
+using namespace std;
+ 
+
+int main()
+{
+    int a, b, c;
+    cout << "Enter the three numbers a, b & c" << endl;
+    cin >> a >> b >> c;
+ 
+    if (a >= b) {
+        if (a >= c) {
+            cout << "The Largest Among Three Numbers is : "
+                 << a << endl;
+        }
+        else {
+            cout << "The Largest Among Three Numbers is : "
+                 << c << endl;
+        }
+    }
+    else {
+        if (b >= c) {
+            cout << "The Largest Among Three Numbers is : "
+                 << b << endl;
+        }
+        else {
+            cout << "The Largest Among Three Numbers is : "
+                 << c << endl;
+        }
+    }
+   
+    return 0;
+}
